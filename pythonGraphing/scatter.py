d0565a93de73a268ef1a9577e7a93469134a9a8a
Kyle Brownell <kyledbrownell@gmail.com>
2018-10-05 22:39:10 -0700

    Adds ice data

--- pythonGraphing/scatter.py
+++ pythonGraphing/scatter.py
@@ -5,7 +5,6 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime as dt
 import matplotlib as mpl
 
 # Column number of each dataset (column numbers start at 0)
@@ -17,16 +16,11 @@ xMinorPerMajorTicks = 10
 # Note: Axis labels are taken from the CSV header
 
 # Read data from file #
-with open('LOG000.CSV', 'rt') as csvfile:
+with open('sittingInLab.CSV', 'rt') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     dataRaw = list(reader)
 headers = dataRaw.pop(0)  # remove headers and save them
 data = np.array(dataRaw)  # convert raw data to array
-
-# Create datetime list from columns 0-5 (yrs-secs) #
-dtList = []
-for i in range (0, len(data[:,0])):
-    dtList.append( dt.datetime( int(data[i,0]),int(data[i,1]),int(data[i,2]),int(data[i,3]),int(data[i,4]),int(data[i,5]) ) )
 
 # Scatter all points from columns A and B #
 fig, ax = plt.subplots()
--- pythonGraphing/timeSeries.py
+++ pythonGraphing/timeSeries.py
@@ -8,23 +8,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 import matplotlib as mpl
-'''
+
 # Axis names
 xAxisLabel = "Amazing X-Axis, blah, blah"
 yAxisLabel = "Neato Y-Axis Label"
 # Number of Y-axis minor ticks per major ticks
 yMinorPerMajorTicks = 10
 # X-Axis minor tick options are: YearLocator, MonthLocator, etc.
-#xMinorTicks = mpl.dates.MinuteLocator()
+xMinorTicks = mpl.dates.SecondLocator()
 # Date formatting options are: %Y %m %d %H %M %S
-xFmt = mpl.dates.DateFormatter('%H:%M')
+xFmt = mpl.dates.DateFormatter('%M:%S')
 # Legend location options are 0-5
 legendLocation = 2
 # Note 1: Series names for the legend are taken from the CSV header
 # Note 2: Major axis ticks are decided automagically by python
-'''
+
 # Read data from file #
-with open('LOG000.CSV', 'rt') as csvfile:
+with open('rtdIce1.CSV', 'rt') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     dataRaw = list(reader)
 headers = dataRaw.pop(0)  # remove headers and save them
@@ -33,31 +33,26 @@ data = np.array(dataRaw)  # convert raw data to array
 # Create datetime list from columns 0-5 (yrs-secs) #
 dtList = []
 for i in range (0, len(data[:,0])):
-    dtList.append( dt.datetime( int(data[i,0]),int(data[i,1]),int(data[i,2]),int(data[i,3]),int(data[i,4]),int(data[i,5]) ) )
+    dtList.append( dt.datetime( int(data[i,0]),int(data[i,1]),int(data[i,2]),
+                                int(data[i,3]),int(data[i,4]),int(data[i,5]) ) )
 
 # Plot sets vs time #
 fig, ax = plt.subplots()
-plt.plot(dtList, data[:,6], label=headers[6])
-'''
-fig, ax = plt.subplots()
 for i in range (6, len(data[1,:])):
     plt.plot(dtList, data[:,i], label=headers[i])
-'''
+
 # Format Axes #
-'''
 plt.xlabel(xAxisLabel)
 plt.ylabel(yAxisLabel)
 plt.legend(loc = legendLocation)    # 0-5
 ax.xaxis.set_major_formatter(xFmt)
-try:
-    ax.xaxis.set_minor_locator(xMinorTicks)
-except:
-    None
+ax.xaxis.set_minor_locator(xMinorTicks)
+
 
 ax.tick_params(axis='both',which='major',length=6)
 ax.tick_params(axis='both',which='minor',length=4)
-ax.yaxis.set_minor_locator(
-    mpl.ticker.AutoMinorLocator(n=yMinorPerMajorTicks));
-fig.autofmt_xdate()'''
+'''ax.yaxis.set_minor_locator(
+    mpl.ticker.AutoMinorLocator(n=yMinorPerMajorTicks));'''
+fig.autofmt_xdate()
 
 plt.show()
